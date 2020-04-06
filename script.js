a024551a043bc9dc2d8033217cba8e245ad2ebbc
NannyMika <sara.majava83@gmail.com>
2020-04-06 17:55:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// task number 1. 
+
+let side = prompt('Write a length of a side');
+console.log(side * 4);
+
+
+
+// task number 2. 
+
+let birth = +prompt('Type your birth year');
+const currentYear = +2020; 
+console.log(currentYear - birth);
+
+// task number 3. 
+
+let km = +prompt('What is the distance in km between your hometown and your current position'); 
+let time = +prompt('In how many hours would you like to be there');
+console.log(km / time); 
+
+
+// task number 4. 
+
+let dollars = +prompt('write any number of dollars'); 
+const rate = +0.93; 
+console.log(dollars * rate); 
+
+
+// task number 5. I didn´t manage to solve this one. 
+
+
+// task number 6. I don´t know if I did this one right, feedback is very welcome. 
+
+let time = prompt('What time is it?');
+
+let hours = time.slice(0, 2); 
+
+let minutes = time.slice(3);
+
+console.log(23 - hours, 60 - minutes); 
+
+
+
+
+
+
+
